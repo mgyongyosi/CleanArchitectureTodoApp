787955e0a33067ee9f3cac3fdb6154c458947850
mgyongyosi <mgyongyosi@users.noreply.github.com>
2017-02-06 20:14:40 +0100

    initial commit, Todo entity added

---
+++ CleanArchitectureTodoApp.Core/Entity/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTodoApp.Core.Entity
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+        public Todo(int id, string title, bool completed)
+        {
+            Id = id;
+            Title = title;
+            Completed = completed;
+        }
+    }
+}
